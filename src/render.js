7ee99225600e025c7b49e7650cd866af9e20b339
Ruslana <lanasigalova@gmail.com>
2026-01-05 16:02:12 -0500

    changing status classes

--- src/render.js
+++ src/render.js
@@ -8,27 +8,30 @@ export default function render(path, value, state) {
   const feedsSection = document.querySelector("#feeds");
   const postsSection = document.querySelector("#posts");
 
-  if (path === "form.error") {
+  if (path === 'form.error') {
     feedback.textContent = i18next.t(`error.${value}`);
-    feedback.classList.remove('text-success')
-    feedback.classList.add('text-danger')
+    feedback.className =
+      'feedback m-0 position-absolute small text-danger';
+    input.classList.add('is-invalid');
     input.classList.add("is-invalid");
     i18next.t("error.invalid");
     input.classList.remove("border-success");
   }
-
-  if (path === "form.status") {
-    input.value = "";
-    feedback.textContent = i18next.t(`status.${value}`);
-    input.classList.remove("is-invalid");
-    input.classList.add("border", "border-3", "border-success");
-    feedback.classList.remove('text-danger'); 
-
-    if(value === 'success') {
-        console.log('ADDING text-success');
-
-        feedback.classList.add('text-success')
-    }
+  if (path === 'form.status' && value === 'sending') {
+    feedback.textContent = i18next.t('status.sending');
+    feedback.className =
+      'feedback m-0 position-absolute small text-secondary';
+  
+    input.classList.remove('is-invalid');
+  }
+  if (path === 'form.status' && value === 'success') {
+    feedback.textContent = i18next.t('status.success');
+    feedback.className =
+      'feedback m-0 position-absolute small text-success';
+  
+    input.classList.remove('is-invalid');
+    input.classList.add('border', 'border-3', 'border-success');
+    input.value = '';
     input.focus();
   }
 
